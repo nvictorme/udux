f80d59aaed91484f9d205647a738a649d3879557
Victor Noguera <noguera.victor@gmail.com>
2024-08-10 15:17:11 -0400

    express and typeorm setup

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,22 +1,50 @@
 import { AppDataSource } from "./data-source";
-import { User } from "./entity/User";
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 AppDataSource.initialize()
   .then(async () => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await AppDataSource.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    // console.log("Inserting a new user into the database...");
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // await AppDataSource.manager.save(user);
+    // console.log("Saved a new user with id: " + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await AppDataSource.manager.find(User);
-    console.log("Loaded users: ", users);
+    // console.log("Loading users from the database...");
+    // const users = await AppDataSource.manager.find(User);
+    // console.log("Loaded users: ", users);
 
-    console.log(
-      "Here you can setup and run express / fastify / any other framework."
-    );
+    // Create a new express application instance
+    const app = express();
+
+    // CORS, JSON, Helmet, and Morgan middleware
+    const corsHandler = cors({
+      origin: "*",
+    });
+    app
+      .use(corsHandler)
+      .use(express.urlencoded({ extended: true }))
+      .use(express.json())
+      .use(morgan("combined"));
+
+    // TODO: Define a route handler
+
+    // 404 - catch all
+    app.use((req, res) => {
+      res.status(404).json({ message: "Not Found" });
+    });
+
+    // Start the server
+    const { APP_NAME, APP_VERSION, APP_PORT } = process.env;
+    const port = APP_PORT || 3000;
+    app.listen(port, () => {
+      console.log(
+        `${APP_NAME} v${APP_VERSION} is running at http://localhost:${port}`
+      );
+    });
   })
   .catch((error) => console.log(error));
