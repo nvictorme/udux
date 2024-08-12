bea3651147c5417bd4054f3724541c0a3c0fbe2f
Victor Noguera <noguera.victor@gmail.com>
2024-08-12 12:58:50 -0400

    run with pm2, app on port 80, backend on port 3000
    remove lerna bootstrap command

--- packages/app/src/config.ts
+++ packages/app/src/config.ts
@@ -1,2 +1,3 @@
-// IP should match the IP of the machine in Tailscale
-export const API_BASE_URL = "http://100.74.60.11:3000";
+// Use Tailscale MagicDNS or IP address for API_BASE_URL
+// and port 3000 or whatever port you are using for the API
+export const API_BASE_URL = "http://nvictor-mbp.taile90e4.ts.net:3000";
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173,
+    port: 80,
   },
 });
