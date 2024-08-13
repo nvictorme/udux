859808d5b78b82764ab5b4c2f92412bd89929db7
Victor Noguera <noguera.victor@gmail.com>
2024-08-13 02:06:03 -0400

    localhost API url

--- packages/app/src/config.ts
+++ packages/app/src/config.ts
@@ -1,3 +1,3 @@
-// Use Tailscale MagicDNS or IP address for API_BASE_URL
+// Use Tailscale MagicDNS, hostname or IP address for API_BASE_URL
 // and port 3000 or whatever port you are using for the API
-export const API_BASE_URL = "http://nvictor-mbp.taile90e4.ts.net:3000";
+export const API_BASE_URL = "http://localhost:3000";
