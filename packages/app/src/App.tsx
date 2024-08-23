3f8c350b02f1a3b7dcf444c5c09fc44567e011c3
Victor Noguera <noguera.victor@gmail.com>
2024-08-23 12:11:15 -0400

    fix texts

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -7,10 +7,10 @@ import { formatDate } from "shared/src/helpers";
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold text-center my-4">
-        Sistema de gestión de consultas médicas UDU
+      <h1 className="text-xl font-bold text-center">
+        Unidad Diagnóstica Urológica
       </h1>
-      <h2 className="text-xl font-bold my-4">
+      <h2 className="font-bold my-4">
         Hoy es {formatDate(new Date().toISOString().split("T")[0])}
       </h2>
       <Tabs defaultValue="citas">
