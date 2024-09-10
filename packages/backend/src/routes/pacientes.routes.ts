ece667a2000de06e2d64d61253614f1a8ff19fef
Victor Noguera <noguera.victor@gmail.com>
2024-09-10 00:02:18 -0400

    cargar citas de manera independiente

--- packages/backend/src/routes/pacientes.routes.ts
+++ packages/backend/src/routes/pacientes.routes.ts
@@ -22,7 +22,7 @@ router.get("/", async (req: Request, res: Response) => {
       }),
       take: parseInt(limit as string),
       skip: (parseInt(page as string) - 1) * parseInt(limit as string),
-      relations: ["antecedentes", "citas"],
+      relations: ["antecedentes"],
       order: { id: "DESC" },
     });
     res.status(200).json({
